0507a7bd168ade811ea3165e1f504d6053d03fe6
Ces4rGlez <ces4rglez17@gmail.com>
2026-06-15 16:42:53 -0600

    chore: setup Wear OS module with Compose for Wear OS dependencies and WearTheme

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "mx.utng.cfga.smarthealthmonitor.wear"
+        applicationId = "mx.utng.cfga.smarthealthmonitor"
         minSdk = 30
         targetSdk = 36
         versionCode = 1
