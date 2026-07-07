092c5cd461242a15d1de11e7f323f92cab79446a
Ces4rGlez <ces4rglez17@gmail.com>
2026-07-07 12:01:52 -0600

    feat: connect TvViewModel to Room DAO and StateFlow for reactive TV UI

---
+++ shared/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "mx.utng.cfga.smarthealthmonitor.shared"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    
+    val roomVersion = "2.8.4"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
